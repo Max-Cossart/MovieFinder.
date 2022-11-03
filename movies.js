967ac5331b6f2edc93da57d8dc70065feef1748a
Max-Cossart <mcossart1998@gmail.com>
2022-11-03 18:05:18 +1100

    updated

--- movies.js
+++ movies.js
@@ -8,30 +8,32 @@ const searchResults = document.querySelector (".search");
 let title;
 
 function searchInput(event) {
-  title = Input.value;
+  title = document.body.querySelector(".movies__search--bar").value;
   if (event.keyCode == 13) {
     searchBtn()
   }
 }
 
 function searchBtn(event) {
-  let searhResultsHTML = `<h2 class="search">Search results for: <span class="color">   ${title} </span> </h2>`;
+  let searhResultsHTML = `<h2 class="search">Search Results for : <span class="color">   ${title} </span> </h2>`;
   searchResults.innerHTML = searhResultsHTML;
   movieSearch(title);
-  
-  
 }
 
 
 
 async function movieSearch(title) {
   const movies = await fetch(
-    `https://www.omdbapi.com/?i=tt3896198&apikey=7840b18e&s=${title}`
+    `http://www.omdbapi.com/?apikey=7840b18e&s=${title}`
   );
   moviesData = await movies.json();
   const films = moviesData.Search.slice(0, 8);
   movieCardEl.innerHTML = films.map((movie) => moviesHTML(movie)).join("");
+
 }
+
+movieSearch()
+
 
 function moviesHTML(movie) {
   return `<div class="movie__card">
@@ -44,9 +46,9 @@ function moviesHTML(movie) {
   </figure>
   <div class="movie__description">
     <h3 class="movie__description--title">
-      ${movie.Title}
+      "${movie.Title}"
     </h3>
-    <h4 class="movie__description--year">Year Released: ${movie.Year}</h4>
+    <h4 class="movie__description--year">Year Released: "${movie.Year}"</h4>
   </div>
   </div>`;
 }
